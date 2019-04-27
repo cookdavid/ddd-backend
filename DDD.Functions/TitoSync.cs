3376fc4a07477964e287a26e56900bb794138f3c
Vatsalya Goel <vatsalya@goel.net.in>
2019-04-27 10:13:59 +1000

    Add tito webhook in .Net

--- DDD.Functions/TitoSync.cs
+++ DDD.Functions/TitoSync.cs
@@ -1,8 +1,3 @@
-using DDD.Core.Tito;
-using DDD.Functions.Extensions;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,93 +5,88 @@ using System.Net.Http;
 using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using DDD.Core.Tito;
+using DDD.Functions.Extensions;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace DDD.Functions
 {
     public static class TitoSync
     {
-        private static TitoSyncConfig _titoSyncConfig;
-        private static HttpClient _httpClient;
-        private static ILogger _logger;
-
         [FunctionName("TitoSync")]
         public static async Task Run(
             [TimerTrigger("%TitoSyncSchedule%")]
             TimerInfo timer,
-            ILogger logger,
+            ILogger log,
             [BindConferenceConfig] ConferenceConfig conference,
             [BindKeyDatesConfig] KeyDatesConfig keyDates,
-            [BindTitoSyncConfig] TitoSyncConfig titoSyncConfig
+            [BindTitoSyncConfig] TitoSyncConfig config
         )
         {
-            _titoSyncConfig = titoSyncConfig;
-            _logger = logger;
-
             if (keyDates.After(x => x.StopSyncingTitoFromDate, TimeSpan.FromMinutes(10)))
             {
-                _logger.LogInformation("Tito sync sync date passed");
+                log.LogInformation("TitoSync sync date passed");
                 return;
             }
-
+            
             var ids = new List<string>();
-            _httpClient = new HttpClient();
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Token", $"token={titoSyncConfig.ApiKey}");
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Token", $"token={config.ApiKey}");
 
-            var (tickets, hasMoreItems, nextPage) = await GetRegistrationsAsync();
-
+            var (tickets, hasMoreItems, nextPage) = await GetTicketsAsync(httpClient, config, log);
+            
             if (tickets != null && tickets.Any())
             {
                 ids.AddRange(tickets.Select(o => o.Id));
-                _logger.LogInformation("Retrieved {registrationsCount} tickets from Tito.", tickets.Count());
+                log.LogInformation("Retrieved {ticketsCount} tickets from Tito.", tickets.Count());
             }
-
+            
             while (hasMoreItems)
             {
-                (tickets, hasMoreItems, nextPage) = await GetRegistrationsAsync(nextPage.Value);
-
+                (tickets, hasMoreItems, nextPage) = await GetTicketsAsync(httpClient, config, log, nextPage.Value);
                 if (tickets != null && tickets.Any())
                 {
-                    _logger.LogInformation("Found more {registrationsCount} tickets from Tito.", tickets.Count());
+                    log.LogInformation("Found more {ticketsCount} tickets from Tito.", tickets.Count());
                     ids.AddRange(tickets.Select(o => o.Id));
                 }
             }
-
-            var repo = await titoSyncConfig.GetRepositoryAsync();
+            
+            var repo = await config.GetRepositoryAsync();
             var existingTickets = await repo.GetAllAsync(conference.ConferenceInstance);
 
             // Taking up to 100 records to meet Azure Storage Bulk Operation limit
             var newTickets = ids.Except(existingTickets.Select(x => x.TicketId).ToArray()).Distinct().Take(100).ToArray();
-            _logger.LogInformation(
+            log.LogInformation(
                 "Found {existingCount} existing tickets and {currentCount} current tickets. Inserting {newCount} new tickets.",
                 existingTickets.Count, ids.Count, newTickets.Count());
             await repo.CreateBatchAsync(newTickets.Select(o => new TitoTicket(conference.ConferenceInstance, o))
                 .ToArray());
         }
 
-
-        private static async Task<(Ticket[], bool, int?)> GetRegistrationsAsync(int pageNumber = 1)
+        private static async Task<(Ticket[], bool, int?)> GetTicketsAsync(HttpClient httpClient, TitoSyncConfig config, ILogger log, int pageNumber = 1)
         {
-            var titoUrl = $"https://api.tito.io/v3/{_titoSyncConfig.AccountId}/{_titoSyncConfig.EventId}/registrations?page={pageNumber}";
-            var response = await _httpClient.GetAsync(titoUrl);
-
-            if (response.IsSuccessStatusCode)
+            var titoUrl = $"https://api.tito.io/v3/{config.AccountId}/{config.EventId}/tickets?page={pageNumber}";
+            var response = await httpClient.GetAsync(titoUrl);
+            if (response.IsSuccessStatusCode) 
             {
                 try
                 {
                     var formatters = new MediaTypeFormatterCollection();
                     formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/vnd.api+json"));
                     var content = await response.Content.ReadAsAsync<PaginatedTitoTicketsResponse>(formatters);
-
+                    
                     return (content.Tickets, content.Meta.HasMoreItems, content.Meta.NextPage);
                 }
-                catch (Exception ex)
+                catch(Exception ex)
                 {
-                    _logger.LogCritical("Error fomratting/reading Tito response. ", ex);
+                    log.LogCritical("Error reading Tito response.", ex);
                 }
             }
-            else
+            else 
             {
-                _logger.LogCritical("Error connecting to Tito with http response: {reason}. The dump of the response: ", response.StatusCode, response.Content.ReadAsStringAsync());
+                log.LogCritical("Error connecting to Tito with http response: {reason}. The dump of the response: ", response.StatusCode, response.Content.ReadAsStringAsync());
             }
             return (null, false, null);
         }
@@ -106,14 +96,14 @@ namespace DDD.Functions
     {
         [JsonProperty("meta")]
         public Meta Meta { get; set; }
-
+        
         [JsonProperty("tickets")]
         public Ticket[] Tickets { get; set; }
     }
 
     public class Ticket
     {
-        [JsonProperty("id")]
+        [JsonProperty("reference")]
         public string Id { get; set; }
     }
 
